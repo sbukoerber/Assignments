0e05562b5be71c17b19036364ea76153d2700e2a
sbukoerber <sbukorber@gmail.com>
2025-01-08 16:59:13 +0530

    Assignment programs

---
+++ src/Q3/Q3_CopyOf.java
@@ -0,0 +1,14 @@
+/*Write a method called copyOf(), which accept an int array and returns a copy of the given array.
+The method's signature is as follows:
+public static int[] copyOf(int[] array)*/
+
+
+package Q3;
+
+import java.util.Arrays;
+
+public class Q3_CopyOf {
+    public static int[] copyOf(int[] array){
+        return Arrays.copyOf(array, array.length);
+    }
+}
